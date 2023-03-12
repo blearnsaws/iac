c364c3fd8249b2888f94273d64393c0a81232ff1
bang <blearnsaws@gmail.com>
2023-03-12 17:27:28 -0500

    added provisioners

--- terraform/deployments/test/products/webserver/main.tf
+++ terraform/deployments/test/products/webserver/main.tf
@@ -1,68 +1,22 @@
 module "ec2_instance" {
   source = "git::https://github.com/blearnsaws/iac.git//terraform/modules/compute/generic-ec2?ref=main"
 
+  for_each = { for ec2 in var.ec2s : ec2.name => ec2 }
+
   ec2 = {
-    ami                         = "ami-006dcf34c09e50022" # AmazonLinux2
-    associate_public_ip_address = true
-    ebs = {
-      device_name = "/dev/sdh"
-      size        = 5
-      type        = "gp2"
-    }
-    instance_type          = "t2.micro"
-    key_name               = "bastion1"
-    name                   = "Bastion-1"
-    subnet_id              = "subnet-0b5193086e82fd626"
-    vpc_security_group_ids = ["sg-004da30f5a9709de3"]
-    tags = {
-      Use          = "Test",
-      Deployed_by  = "Terraform"
-      Project_code = "001"
-    }
-    user_data       = <<-EOF
-        #!/bin/bash
-        yum update -y
-        yum install httpd -y
-        echo 'If you can read this, the apache server is up and working!!' > /var/www/html/index.html
-        systemctl enable --now httpd
-        EOF
-    rootvolume_size = 8
-    rootvolume_type = "gp2"
-    sg_ingress = [
-      {
-        description       = "SSH ingress rule"
-        cidr_blocks       = ["0.0.0.0/0"] # Not good practice to open to the world
-        from_port         = 22
-        protocol          = "tcp"
-        to_port           = 22
-        security_group_id = "sg-004da30f5a9709de3"
-      },
-      {
-        description       = "RDP ingress rule"
-        cidr_blocks       = ["0.0.0.0/0"] # Not good practice to open to the world
-        from_port         = 3389
-        protocol          = "tcp"
-        to_port           = 3389
-        security_group_id = "sg-004da30f5a9709de3"
-      },
-      {
-        description       = "HTTP ingress rule"
-        cidr_blocks       = ["0.0.0.0/0"] # Not good practice to open to the world
-        from_port         = 80
-        protocol          = "tcp"
-        to_port           = 80
-        security_group_id = "sg-004da30f5a9709de3"
-      }
-    ]
-    sg_egress = [
-      {
-        description       = "ALL egress rule"
-        cidr_blocks       = ["0.0.0.0/0"] # Not good practice to open to the world
-        from_port         = 0
-        protocol          = "-1"
-        to_port           = 0
-        security_group_id = "sg-004da30f5a9709de3"
-      }
-    ]
+    ami                         = each.value.ami
+    associate_public_ip_address = each.value.associate_public_ip_address
+    ebs                         = each.value.ebs
+    instance_type               = each.value.instance_type
+    key_name                    = each.value.key_name
+    name                        = each.value.name
+    subnet_id                   = each.value.subnet_id
+    vpc_security_group_ids      = each.value.vpc_security_group_ids
+    tags                        = each.value.tags
+    user_data                   = each.value.user_data
+    rootvolume_size             = each.value.rootvolume_size
+    rootvolume_type             = each.value.rootvolume_type
+    sg_egress                   = each.value.sg_egress
+    sg_ingress                  = each.value.sg_ingress
   }
 }
