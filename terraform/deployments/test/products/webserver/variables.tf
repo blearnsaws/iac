91696f9c4a991bcf995bf0b76503b85b0afa2034
bang <blearnsaws@gmail.com>
2023-03-12 17:44:10 -0500

    updated test deployments

---
+++ terraform/deployments/test/products/webserver/variables.tf
@@ -0,0 +1,36 @@
+variable "ec2s" {
+  type = list(object({
+    ami                         = string
+    associate_public_ip_address = bool
+    ebs = object({
+      device_name = string
+      size        = number
+      type        = string
+    })
+    instance_type          = string
+    key_name               = string
+    name                   = string
+    vpc_security_group_ids = list(string)
+    subnet_id              = string
+    tags                   = map(string)
+    user_data              = string
+    rootvolume_size        = number
+    rootvolume_type        = string
+    sg_ingress = list(object({
+      description       = string
+      cidr_blocks       = list(string)
+      from_port         = number
+      protocol          = string
+      to_port           = number
+      security_group_id = string
+    }))
+    sg_egress = list(object({
+      description       = string
+      cidr_blocks       = list(string)
+      from_port         = number
+      protocol          = string
+      to_port           = number
+      security_group_id = string
+    }))
+  }))
+}
