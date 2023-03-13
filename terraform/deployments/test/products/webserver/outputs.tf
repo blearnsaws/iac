509605df1b692f2f441192586849e468c83d4730
bang <blearnsaws@gmail.com>
2023-03-12 22:57:13 -0500

    added outputs

--- terraform/deployments/test/products/webserver/outputs.tf
+++ terraform/deployments/test/products/webserver/outputs.tf
@@ -1,3 +1,8 @@
+output "security_group" {
+  value = module.security_group
+}
+
 output "ec2_instance_id" {
   value = module.ec2_instance[*]
 }
+
--- terraform/modules/network/security_group/outputs.tf
+++ terraform/modules/network/security_group/outputs.tf
@@ -1,3 +1,8 @@
-output "security_group_id" {
+output "security_group_id_id" {
     value = resource.aws_security_group.default_sg.id
 }
+
+output "security_group_id_all" {
+    value = resource.aws_security_group.default_sg
+}
+
